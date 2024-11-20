d880dc13fdf988a87504b5849bb828a2987258eb
Aaron Cooper <snazzie@snazzie.space>
2024-11-20 18:55:11 +0000

    benchmark

---
+++ BenchmarkTryCatch/Program.cs
@@ -0,0 +1,98 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+BenchmarkRunner.Run<TryCatchOverHead>();
+
+public class TryCatchOverHead()
+{
+
+    [Benchmark]
+    public int? TryCatch_NoError()
+    {
+
+        try
+        {
+            var result =  ComputeWithOption(1);
+            return result.num;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+
+    }
+    
+    [Benchmark]
+    public int? NoTryCatch_NoError()
+    {
+        var result =  ComputeWithOption(1);
+        return result.num;
+    }
+    
+    [Benchmark]
+    public int? TryCatch_WithErrorOption()
+    {
+
+        try
+        {
+            var result =ComputeWithOption(0);
+            return result.num;
+        }
+        catch (Exception e)
+        {
+
+            return null;
+        }
+    }
+    
+    [Benchmark]
+    public int? TryCatch_WithThrow()
+    {
+        try
+        {
+            var result = ComputeWithThrow(0);
+            return result.num;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+    
+    (int? num, string? err) ComputeWithOption(int divideBy)
+    {
+        int? num = null;
+
+        num = Compute();
+        if (divideBy == 0)
+            return (num, "Cannot divide by 0");
+        
+        num = num / divideBy;
+  
+        return (num, null);
+    }
+    
+    (int? num, string? err) ComputeWithThrow(int divideBy)
+    {
+        int? num = null;
+        num = Compute();
+        
+        num = num / divideBy;
+  
+        return (num, null);
+    }
+    
+    
+    int Compute()
+    {
+        var num = 0;
+        foreach (var item in Enumerable.Range(0, 200))
+        {
+            num = item;
+        }
+        
+        return num;
+    }
+}
